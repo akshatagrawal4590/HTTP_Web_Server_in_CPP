337fce37dff186a3a3827b51d329f3cbe7b86b08
akshatagrawal4590 <akshatagrawal4590@gmail.com>
2023-07-12 19:48:25 +0530

    Modified the listen functon to remove the bug and implemented some helper classes

--- L-05/bro.cpp
+++ L-05/bro.cpp
@@ -15,6 +15,63 @@ using namespace std;
 
 // Amit (The Bro Programmer)
 
+class StringUtility
+{
+    private:
+    StringUtility(){}
+
+    public:
+    static void toLowerCase(char *str)
+    {
+        if(str == NULL)
+        {
+            return;
+        }
+        for(; *str != '\0'; str++)
+        {
+            if(*str >= 65 && *str <= 90)
+            {
+                *str += 32;
+            }
+        }
+    }
+};
+
+class HTTPErrorStatusUtility
+{
+    private:
+    HTTPErrorStatusUtility(){}
+
+    public:
+    static void sendBadRequestError(int clientSocketDescriptor)
+    {
+        // Will complete later on
+    }
+
+    static void sendHttpVersionNotSupportedError(int clientSocketDescriptor, char* httpVersion)
+    {
+        // Will complete later on
+    }
+
+    static void sendNotFoundError(int clientSocketDescriptor, char *requestURL)
+    {
+        char content[1000];
+        char header[200];
+        char response[1200];
+        sprintf(content, "<!doctype html><html lang='en'><head><meta charset='utf-8'><title>404 Error</title></head><body>Requested Resource [%s] not found</body>", requestURL);
+        int contentLength = strlen(content);
+        sprintf(header, "HTTP/1.1 404 Not Found\r\nContent-Type:text/html\nContent-Length:%d\nConnection: close\r\n\r\n", contentLength);
+        strcpy(response, header);
+        strcat(response, content);
+        send(clientSocketDescriptor, response, strlen(response), 0);
+    }
+
+    static void sendMethodNotAllowedError(int clientSocketDescriptor, char *method, char *requestURL)
+    {
+        // Will complete later on
+    }
+};
+
 class Validator
 {
     private:
@@ -67,7 +124,18 @@ class Error
 
 class Request
 {
+    private:
+    char *method;
+    char *requestURL;
+    char *httpVersion;
+    Request(char *method, char *requestURL, char *httpVersion)
+    {
+        this -> method = method;
+        this -> requestURL = requestURL;
+        this -> httpVersion = httpVersion;
+    }
 
+    friend class Bro;
 };
 
 class Response
@@ -103,6 +171,30 @@ class Response
         this -> contentLength += content.length();
         this -> contentIterator = this -> content.insert_after(this -> contentIterator, content);
         return *this;
+    }
+
+    friend class HTTPResponseUtility;
+};
+
+class HTTPResponseUtility
+{
+    private:
+    HTTPResponseUtility(){}
+
+    public:
+    static void sendResponse(int clientSocketDescriptor, Response &response)
+    {
+        char header[200];
+        int contentLength = response.contentLength;
+        sprintf(header, "HTTP/1.1 200 OK\r\nContent-Type:text/html\nContent-Length:%d\nConnection: close\r\n\r\n", contentLength);
+        send(clientSocketDescriptor, header, strlen(header), 0);
+        auto contentIterator = response.content.begin();
+        while(contentIterator != response.content.end())
+        {
+            string str = *contentIterator;
+            send(clientSocketDescriptor, str.c_str(), str.length(), 0);
+            ++contentIterator;
+        }
     }
 };
 
@@ -215,57 +307,132 @@ class Bro
         while(1)
         {
             clientSocketDescriptor = accept(serverSocketDescriptor, (struct sockaddr *)&clientSocketInformation, &len);
-            if(clientSocketDescriptor < 0)
+            requestLength = recv(clientSocketDescriptor, requestBuffer, sizeof(requestBuffer) - sizeof(char), 0);
+            if(requestLength == 0 || requestLength == -1)
             {
-                // Not yet decided
+                close(clientSocketDescriptor);
+                continue;
             }
-            forward_list<string> requestBufferDS;
-            forward_list<string> :: iterator requestBufferDSIterator;
-            requestBufferDSIterator = requestBufferDS.before_begin();
-            int requestBufferDSSize = 0;
-            int requestDataCount = 0;
-            while (1)
-            {
-                requestLength = recv(clientSocketDescriptor, requestBuffer, sizeof(requestBuffer) - sizeof(char), 0);
-                if(requestLength == 0)
-                {
-                    break;
-                }         
-                requestBuffer[requestLength] = '\0';
-                requestBufferDSIterator = requestBufferDS.insert_after(requestBufferDSIterator, string(requestBuffer));       
-                requestBufferDSSize++;
-                requestDataCount += requestLength;
-            }
+            int i;
+            char *method, *requestURL, *httpVersion;
+            requestBuffer[requestLength] = '\0';
+
+            //Code to parse the first line of the http request starts here
             
-            if(requestBufferDSSize > 0)
+            //First line should be REQUEST_METHOD SPACE URI SPACE HTTPVersion CRLF
+            method = requestBuffer;
+            i = 0;
+            while(requestBuffer[i] && requestBuffer[i] != ' ')
             {
-                char *requestData = new char[requestDataCount+1];
-                char *p = requestData;
-                const char *q;
-                requestBufferDSIterator = requestBufferDS.begin();
-                while(requestBufferDSIterator != requestBufferDS.end())
-                {
-                    q = (*requestBufferDSIterator).c_str();
-                    while(*q)
-                    {
-                        *p = *q;
-                        p++;
-                        q++;
-                    }
-                    ++requestBufferDSIterator;
-                }
-                *p = '\0';
-                requestBufferDS.clear();
-                cout<<"----- Request data begin -----"<<endl;
-                cout<<requestData<<endl;
-                cout<<"----- Request data end -------"<<endl;
-                // the code to parse the request goes here
-                delete [] requestData;
+                i++;
+            }
+            if(requestBuffer[i] == '\0')
+            {
+                HTTPErrorStatusUtility :: sendBadRequestError(clientSocketDescriptor);
+                close(clientSocketDescriptor);
+                continue;
+            }
+            requestBuffer[i] = '\0';
+            i++;
+            if(requestBuffer[i] == ' ' || requestBuffer[i] == '\0')
+            {
+                HTTPErrorStatusUtility :: sendBadRequestError(clientSocketDescriptor);
+                close(clientSocketDescriptor);
+                continue;
+            }
+            StringUtility :: toLowerCase(method);
+            if(!(strcmp(method, "get") == 0 || strcmp(method, "post") == 0 || strcmp(method, "put") == 0 || strcmp(method, "connect") == 0 || strcmp(method, "options") == 0 || strcmp(method, "trace") == 0 || strcmp(method, "head") == 0 || strcmp(method, "delete") == 0))
+            {
+                HTTPErrorStatusUtility :: sendBadRequestError(clientSocketDescriptor);
+                close(clientSocketDescriptor);
+                continue;
+            }
+
+            //If the control reaches here that means the request method has been parsed
+            
+            requestURL = requestBuffer + i;
+            while(requestBuffer[i] && requestBuffer[i] != ' ')
+            {
+                i++;
+            }
+            if(requestBuffer[i] == '\0')
+            {
+                HTTPErrorStatusUtility :: sendBadRequestError(clientSocketDescriptor);
+                close(clientSocketDescriptor);
+                continue;
+            }
+            requestBuffer[i] = '\0';
+            i++;
+            if(requestBuffer[i] == ' ' || requestBuffer[i] == '\0')
+            {
+                HTTPErrorStatusUtility :: sendBadRequestError(clientSocketDescriptor);
+                close(clientSocketDescriptor);
+                continue;
+            }
+
+            //If the control reaches here that means the requestURL has been parsed
+
+            httpVersion = requestBuffer + i;
+            while(requestBuffer[i] && requestBuffer[i] != '\r' && requestBuffer[i] != '\n')
+            {
+                i++;
+            }
+            if(requestBuffer[i] == '\0')
+            {
+                HTTPErrorStatusUtility :: sendBadRequestError(clientSocketDescriptor);
+                close(clientSocketDescriptor);
+                continue;
+            }
+            if(requestBuffer[i] == '\r' && requestBuffer[i+1] != '\n')
+            {
+                HTTPErrorStatusUtility :: sendBadRequestError(clientSocketDescriptor);
+                close(clientSocketDescriptor);
+                continue;
+            }
+            if(requestBuffer[i] == '\r')
+            {
+                requestBuffer[i] = '\0';
+                i = i + 2;
             }
             else
             {
-                // Something if no data is recieved
+                requestBuffer[i] = '\0';
+                i = i + 1;
             }
+            StringUtility :: toLowerCase(httpVersion);
+            if(strcmp(httpVersion, "http/1.1") != 0)
+            {
+                HTTPErrorStatusUtility :: sendHttpVersionNotSupportedError(clientSocketDescriptor, httpVersion);
+                close(clientSocketDescriptor);
+                continue;
+            }
+
+            //If the control reaches here that means the first line have been parsed
+            
+            auto urlMappingIterator = urlMappings.find(requestURL);
+            if(urlMappingIterator == urlMappings.end())
+            {
+                HTTPErrorStatusUtility :: sendNotFoundError(clientSocketDescriptor, requestURL);
+                close(clientSocketDescriptor);
+                continue;
+            }
+            URLMapping urlMapping = urlMappingIterator -> second;
+            if(urlMapping.requestMethod == __GET__ && strcmp(method, "get") != 0)
+            {
+                HTTPErrorStatusUtility :: sendMethodNotAllowedError(clientSocketDescriptor, method, httpVersion);
+                close(clientSocketDescriptor);
+                continue;
+            }
+
+            //Code to parse the header and then the payload if exists starts here
+            //Code to parse the header and then the payload if exists ends here
+
+            Request request (method, requestURL, httpVersion);
+            Response response;
+            urlMapping.mappedFunction(request, response);
+            HTTPResponseUtility :: sendResponse(clientSocketDescriptor, response);
+
+
             close(clientSocketDescriptor);
         }
     }
@@ -288,7 +455,7 @@ int main()
             </head>
             <body>
             <h1>Welcome</h1>
-            <h3>Sone Text</h3>
+            <h3>Some Text</h3>
             <a href='getCustomers'>Customers List</a>
             </body>
             </html>
